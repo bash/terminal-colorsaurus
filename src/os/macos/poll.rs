c4d24a4a55ab8d8043497bd9e0ff28030a68eec9
Tau <git@tau.garden>
2024-01-23 13:05:57 +0100

    Fix macos poll

--- src/os/macos/poll.rs
+++ src/os/macos/poll.rs
@@ -2,13 +2,13 @@ use crate::{Error, Result};
 use libc::{c_int, pselect, time_t, timespec, FD_ISSET, FD_SET};
 use std::io;
 use std::mem::zeroed;
+use std::os::fd::{AsRawFd as _, BorrowedFd};
 use std::ptr::{null, null_mut};
 use std::time::Duration;
-use terminal_trx::Transceive;
 
 // macOS does not support polling /dev/tty using kqueue, so we have to
 // resort to pselect/select. See https://nathancraddock.com/blog/macos-dev-tty-polling/.
-pub(crate) fn poll_read(terminal: &dyn Transceive, timeout: Duration) -> Result<()> {
+pub(crate) fn poll_read(terminal: BorrowedFd, timeout: Duration) -> Result<()> {
     let fd = terminal.as_raw_fd();
     let timespec = to_timespec(timeout);
     // SAFETY: A zeroed fd_set is valid (FD_ZERO zeroes an existing fd_set so this state must be fine).
