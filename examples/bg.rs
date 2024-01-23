67743051cfccbf446fedb9299226453dbc440c8e
Tau <git@tau.garden>
2024-01-23 13:01:54 +0100

    Replace unwrap with try

--- examples/bg.rs
+++ examples/bg.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 use terminal_colorsaurus::{background_color, QueryOptions};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let fg = background_color(QueryOptions::default()).unwrap();
+    let fg = background_color(QueryOptions::default())?;
     println!("rgb({}, {}, {})", fg.r >> 8, fg.g >> 8, fg.b >> 8);
     Ok(())
 }
--- examples/fg.rs
+++ examples/fg.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 use terminal_colorsaurus::{foreground_color, QueryOptions};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let fg = foreground_color(QueryOptions::default()).unwrap();
+    let fg = foreground_color(QueryOptions::default())?;
     println!("rgb({}, {}, {})", fg.r >> 8, fg.g >> 8, fg.b >> 8);
     Ok(())
 }
