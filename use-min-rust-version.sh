c3d12741f3b91e64f1d9c73c66568447459bebc1
Tau Gärtli <git@tau.garden>
2024-05-17 21:01:44 +0200

    Include rust version in job name

--- use-min-rust-version.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-rust_version=$(cargo metadata --format-version 1 --no-deps | jq -r '.packages | map(select(.name == "terminal-colorsaurus")) | first | .rust_version')
-rustup override set "$rust_version"
