6689bbb29d16e523ea7fc8e42e69ab0e1882b49d
Tau Gärtli <git@tau.garden>
2024-06-10 13:06:33 +0200

    Deny warnings

--- docs.sh
+++ docs.sh
@@ -6,7 +6,7 @@ metadata=$(cargo metadata --format-version 1 --no-deps | jq '.packages | map(sel
 features=$(echo "$metadata" | jq -r '.features | join(",")')
 
 export RUSTDOCFLAGS
-RUSTDOCFLAGS=$(echo "$metadata" | jq -r '.["rustdoc-args"] | join(" ")')
+RUSTDOCFLAGS="$RUSTDOCFLAGS $(echo "$metadata" | jq -r '.["rustdoc-args"] | join(" ")')"
 echo "+ RUSTDOCFLAGS=$RUSTDOCFLAGS" > /dev/stderr
 echo "+ cargo doc ... --features $features" > /dev/stderr
 cargo +nightly doc -Zunstable-options -Zrustdoc-scrape-examples --features "$features"
