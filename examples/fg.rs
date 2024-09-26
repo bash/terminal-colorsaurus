0306133f115202116327966fce727101d39afb55
Tau Gärtli <git@tau.garden>
2024-09-26 23:55:25 +0200

    Provide separate `_with_options` overloads

--- examples/fg.rs
+++ examples/fg.rs
@@ -1,9 +1,9 @@
 //! This example shows how to retrieve the terminal's foreground color.
 
-use terminal_colorsaurus::{foreground_color, Error, QueryOptions};
+use terminal_colorsaurus::{foreground_color, Error};
 
 fn main() -> Result<(), display::DisplayAsDebug<Error>> {
-    let fg = foreground_color(QueryOptions::default())?;
+    let fg = foreground_color()?;
     let fg_8bit = fg.scale_to_8bit();
     println!("rgb16({}, {}, {})", fg.r, fg.g, fg.b);
     println!("rgb8({}, {}, {})", fg_8bit.0, fg_8bit.1, fg_8bit.2);
--- examples/theme.rs
+++ examples/theme.rs
@@ -1,10 +1,10 @@
 //! This example shows how to detect if the terminal uses
 //! a dark-on-light or a light-on-dark theme.
 
-use terminal_colorsaurus::{color_palette, ColorScheme, Error, QueryOptions};
+use terminal_colorsaurus::{color_palette, ColorScheme, Error};
 
 fn main() -> Result<(), display::DisplayAsDebug<Error>> {
-    let colors = color_palette(QueryOptions::default())?;
+    let colors = color_palette()?;
 
     let theme = match colors.color_scheme() {
         ColorScheme::Dark => "dark",
