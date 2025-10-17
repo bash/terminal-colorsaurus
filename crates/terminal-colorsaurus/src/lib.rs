3d87d6199ad79ec90b96c40f197a484ef855190a
Tau Gärtli <git@tau.garden>
2025-10-17 20:26:19 +0200

    Remove doc_auto_cfg feature

--- crates/terminal-colorsaurus/src/lib.rs
+++ crates/terminal-colorsaurus/src/lib.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 //! Determines the background and foreground color of the terminal
 //! using the `OSC 10` and `OSC 11` terminal sequence.
